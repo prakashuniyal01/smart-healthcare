97cbaf49c363648f410e141f55af5d5163327d47
prakashuniyal01 <uniyalprakash7@gmail.com>
2025-01-21 00:20:23 +0530

    ridefine

--- apps/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import UserRegistrationView, VerifyOTPView,UserLoginView
-
-urlpatterns = [
-    path('register/', UserRegistrationView.as_view(), name='register'),
-    path('verify-otp/', VerifyOTPView.as_view(), name='verify-otp'),
-    path('login/', UserLoginView.as_view(), name='login'),
-]
--- main.py
+++
@@ -1,3 +0,0 @@
-import secrets
-secret_key = secrets.token_hex(32)  # Generates a random 64-character string
-print(secret_key)
